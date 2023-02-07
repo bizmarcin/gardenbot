4869b7837ce576085a816c9992679b6ad7dc0990
marcin kujawa <kujawa.marcin@outlook.com>
2023-02-07 22:46:14 +0100

    `Movve to another class`

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import openai
 import env_variables
-
+import openai_class
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
@@ -9,23 +8,10 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     import os
-    import openai
+    import openai_class
 
-    openai.api_key = env_variables.variables.OPEN_AI_KEY
+    print(openai_class.oai.question("What about carrot?"))
 
-    gpt_prompt = "Nalewak nb02 nie działa, czy to kwestia aplikacji?"
-
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=gpt_prompt,
-        temperature=0.5,
-        max_tokens=1000,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
-
-    print(response['choices'][0]['text'])
 
 
 
---
+++ openai_class.py
@@ -0,0 +1,17 @@
+import openai
+import env_variables
+class oai:
+    def question(gpt_prompt):
+        openai.api_key = env_variables.variables.OPEN_AI_KEY
+
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=gpt_prompt,
+            temperature=0.5,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+
+        return response['choices'][0]['text']
