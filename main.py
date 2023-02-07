e08af45dca9efcd3e66dadf30e2b477eaebd2046
marcin kujawa <kujawa.marcin@outlook.com>
2023-02-07 22:25:39 +0100

    `Add OPENAI API`

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+import openai
+import env_variables
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    import os
+    import openai
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    openai.api_key = env_variables.variables.OPEN_AI_KEY
+
+    gpt_prompt = "Nalewak nb02 nie działa, czy to kwestia aplikacji?"
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=gpt_prompt,
+        temperature=0.5,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+
+    print(response['choices'][0]['text'])
+
+
+
+
+
+
