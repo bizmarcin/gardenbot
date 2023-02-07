9db2c2d90fb835e5836c33cf3cea5fc5a43a563a
marcin kujawa <kujawa.marcin@outlook.com>
2023-02-07 23:28:34 +0100

    `cleen up code`

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import env_variables
+#definitions
 import openai_class
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+#functions
 
+#main
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    import os
-    import openai_class
-
     print(openai_class.oai.question("What about carrot?"))
 
 
